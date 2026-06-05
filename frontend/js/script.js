1bcba063f523ba6629ccc3c4117e975f1ca2a56a
Alikanti Nivya <nivyaalikanti@gmail.com>
2026-06-05 12:06:25 +0530

    created frontend forms

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,150 +1,171 @@
 document
-.getElementById("predictBtn")
-.addEventListener("click", async () => {
+    .getElementById("predictBtn")
+    .addEventListener("click", async () => {
 
-    const data = {
+        const data = {
 
-        brand:
-        document.getElementById("brand").value,
+            brand:
+                document.getElementById("brand").value,
 
-        os:
-        document.getElementById("os").value,
+            os:
+                document.getElementById("os").value,
 
-        screen_size:
-        Number(
-            document.getElementById("screen_size").value
-        ),
+            screen_size:
+                Number(
+                    document.getElementById("screen_size").value
+                ),
 
-        weight:
-        Number(
-            document.getElementById("weight").value
-        ),
+            weight:
+                Number(
+                    document.getElementById("weight").value
+                ),
 
-        ram:
-        Number(
-            document.getElementById("ram").value
-        ),
+            ram:
+                Number(
+                    document.getElementById("ram").value
+                ),
 
-        internal_memory:
-        Number(
-            document.getElementById("storage").value
-        ),
+            internal_memory:
+                Number(
+                    document.getElementById("storage").value
+                ),
 
-        battery:
-        Number(
-            document.getElementById("battery").value
-        ),
+            battery:
+                Number(
+                    document.getElementById("battery").value
+                ),
 
-        days_used:
-        Number(
-            document.getElementById("days_used").value
-        ),
+            days_used:
+                Number(
+                    document.getElementById("days_used").value
+                ),
 
-        rear_camera_mp:
-        Number(
-            document.getElementById("rear_camera").value
-        ),
+            rear_camera_mp:
+                Number(
+                    document.getElementById("rear_camera").value
+                ),
 
-        front_camera_mp:
-        Number(
-            document.getElementById("front_camera").value
-        ),
+            front_camera_mp:
+                Number(
+                    document.getElementById("front_camera").value
+                ),
 
-        release_year:
-        Number(
-            document.getElementById("release_year").value
-        ),
+            release_year:
+                Number(
+                    document.getElementById("release_year").value
+                ),
 
-        normalized_new_price:
-        Number(
-            document.getElementById("new_price").value
-        ),
+            normalized_new_price:
+                Number(
+                    document.getElementById("new_price").value
+                ),
 
-        fourg:
-        Number(
-            document.getElementById("fourg").value
-        ),
+            fourg:
+                Number(
+                    document.getElementById("fourg").value
+                ),
 
-        fiveg:
-        Number(
-            document.getElementById("fiveg").value
-        )
-    };
+            fiveg:
+                Number(
+                    document.getElementById("fiveg").value
+                )
+        };
 
-    try {
+        try {
 
-        const response = await fetch(
-            "http://127.0.0.1:5000/predict",
-            {
-                method: "POST",
+            const response = await fetch(
+                "http://127.0.0.1:5000/predict",
+                {
+                    method: "POST",
 
-                headers: {
-                    "Content-Type":
-                    "application/json"
-                },
+                    headers: {
+                        "Content-Type":
+                            "application/json"
+                    },
 
-                body:
-                JSON.stringify(data)
-            }
-        );
+                    body:
+                        JSON.stringify(data)
+                }
+            );
 
-        const result =
-        await response.json();
+            const result =
+                await response.json();
 
-        document
-        .getElementById("resaleValue")
-        .innerHTML =
-        "₹ " +
-        result.resale_value;
+            document
+                .getElementById("resaleValue")
+                .innerHTML =
+                "₹ " +
+                result.resale_value;
 
-        document
-        .getElementById("recycleValue")
-        .innerHTML =
-        "₹ " +
-        result.recycling_value;
+            document
+                .getElementById("recycleValue")
+                .innerHTML =
+                "₹ " +
+                result.recycling_value;
 
-        document
-        .getElementById("recommendation")
-        .innerHTML =
-        result.recommendation;
+            document
+                .getElementById("recommendation")
+                .innerHTML =
+                result.recommendation;
 
-        document
-        .getElementById("gold")
-        .innerHTML =
-        "Gold : " +
-        result.gold +
-        " g";
+            document
+                .getElementById("gold")
+                .innerHTML =
+                "Gold : " +
+                result.gold +
+                " g";
 
-        document
-        .getElementById("silver")
-        .innerHTML =
-        "Silver : " +
-        result.silver +
-        " g";
+            document
+                .getElementById("silver")
+                .innerHTML =
+                "Silver : " +
+                result.silver +
+                " g";
 
-        document
-        .getElementById("copper")
-        .innerHTML =
-        "Copper : " +
-        result.copper +
-        " g";
+            document
+                .getElementById("copper")
+                .innerHTML =
+                "Copper : " +
+                result.copper +
+                " g";
 
-        document
-        .getElementById("lithium")
-        .innerHTML =
-        "Lithium : " +
-        result.lithium +
-        " g";
+            document
+                .getElementById("lithium")
+                .innerHTML =
+                "Lithium : " +
+                result.lithium +
+                " g";
 
-    }
+            document
+                .getElementById("co2Saved")
+                .innerHTML =
+                "CO₂ Saved : " +
+                result.co2_saved +
+                " kg";
 
-    catch(error){
+            document
+                .getElementById("landfillWaste")
+                .innerHTML =
+                "Landfill Waste Prevented : " +
+                result.landfill_waste_prevented +
+                " kg";
 
-        console.error(error);
+            document
+                .getElementById("totalRecovered")
+                .innerHTML =
+                "Total Material Recovered : " +
+                result.total_material_recovered +
+                " g";
 
-        alert(
-            "Backend connection failed."
-        );
-    }
+        }
 
-});
+        catch (error) {
+
+            console.error(error);
+
+            alert(
+                "Backend connection failed."
+            );
+        }
+
+    });
